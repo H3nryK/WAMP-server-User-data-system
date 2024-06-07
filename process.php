4a31d7f093ffbb3e6f3cbb858bf95b5d4243969d
Henry Kimani <121490662+H3nryK@users.noreply.github.com>
2024-06-07 22:00:50 +0300

    Id

--- process.php
+++ process.php
@@ -119,6 +119,7 @@ if (isset($_GET['search'])) {
     <!-- Display user data -->
     <table>
         <tr>
+            <th>ID</th>
             <th>Fullname</th>
             <th>Email</th>
             <th>Age</th>
@@ -127,13 +128,14 @@ if (isset($_GET['search'])) {
         if ($result->num_rows > 0) {
             while($row = $result->fetch_assoc()) {
                 echo "<tr>";
+                echo "<td>" . $row["id"] . "</td>";
                 echo "<td>" . $row["name"] . "</td>";
                 echo "<td>" . $row["email"] . "</td>";
                 echo "<td>" . $row["age"] . "</td>";
                 echo "</tr>";
             }
         } else {
-            echo "<tr><td colspan='3'>No data found</td></tr>";
+            echo "<tr><td colspan='4'>No data found</td></tr>";
         }
         ?>
     </table>
